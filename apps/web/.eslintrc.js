0c218156cb4aa8956ead1d2fba53270d100a7dfb
hsskey <slingshot800@gmail.com>
2024-08-20 00:21:29 +0900

    ✨ feat: storybook에 msw-storybook-addon 추가 및 orval로 api 모킹 자동화 적용
    #

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,9 +1,10 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/next.js"],
-  parser: "@typescript-eslint/parser",
+  extends: ['@repo/eslint-config/next.js', 'plugin:storybook/recommended'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: true,
+    project: true
   },
-};
+  ignorePatterns: ['src/generated/**/*.ts']
+}
